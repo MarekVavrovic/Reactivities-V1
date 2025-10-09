d81f8076698deb7554f2107fe80337bde6bac5fe
MarekVavrovic <vavrovic@gmail.com>
2025-10-09 16:30:12 +0100

    end of section 8

---
+++ client/src/features/activities/details/ActivityDetailPage.tsx
@@ -0,0 +1,29 @@
+import { Grid2, Typography } from "@mui/material";
+import { useParams } from "react-router";
+import { UseActivities } from "../../../lib/hooks/UseActivities";
+import ActivityDetailsHeader from "./ActivityDetailsHeader";
+import ActivityDetailsInfo from "./ActivityDetailsInfo";
+import ActivityDetailsChat from "./ActivityDetailsChat";
+import ActivityDetailsSidebar from "./ActivityDetailsSidebar";
+
+export default function ActivityDetailPage() {
+  const { id } = useParams();
+  const { activity, isLoadingActivity } = UseActivities(id);
+
+  if (isLoadingActivity) return <Typography>Loading...</Typography>;
+
+  if (!activity) return <Typography>Activity not found</Typography>;
+
+  return (
+    <Grid2 container spacing={3}>
+      <Grid2 size={8}>
+        <ActivityDetailsHeader activity={activity} />
+        <ActivityDetailsInfo activity={activity} />
+        <ActivityDetailsChat />
+      </Grid2>
+      <Grid2 size={4}>
+        <ActivityDetailsSidebar />
+      </Grid2>
+    </Grid2>
+  );
+}
