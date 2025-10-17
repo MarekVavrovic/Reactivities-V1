48ae9cd46ce619fc7c45fdb0dbd290f64070dcbd
MarekVavrovic <vavrovic@gmail.com>
2025-10-17 17:37:40 +0100

    end of section 11

--- Application/Activities/DTOs/BaseActivityDto.cs
+++ Application/Activities/DTOs/BaseActivityDto.cs
@@ -4,7 +4,7 @@ namespace Application.Activities.DTOs;
 
 public class BaseActivityDto
 {
-     public string Title { get; set; } = "";
+    public string Title { get; set; } = "";
     public DateTime Date { get; set; }
     public string Description { get; set; } = string.Empty;
     public string Category { get; set; } = "";
--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -1,77 +1,86 @@
-import { Box, Button, Paper, TextField, Typography } from "@mui/material";
-import type { FormEvent } from "react";
+import { Box, Button, Paper, Typography } from "@mui/material";
 import { UseActivities } from "../../../lib/hooks/UseActivities";
 import { useNavigate, useParams } from "react-router";
+import { useForm} from "react-hook-form";
+import { useEffect } from "react";
+import type { ActivitySchema } from "../../../lib/schemas/activitySchema";
+import { activitySchema } from "../../../lib/schemas/activitySchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import TextInput from "../../../app/shared/components/TextInput";
+import SelectInput from "../../../app/shared/components/SelectInput";
+import { categoryOptions } from "./categoryOptions";
+import DateTimeInput from "../../../app/shared/components/DateTimeInput";
+import LocationInput from "../../../app/shared/components/LocationInput";
 
 export default function ActivityForm() {
-  const { id } = useParams();
-  const { updateActivity, createActivity, activity, isLoadingActivity } =
-    UseActivities(id);
+  const {
+    reset,
+    handleSubmit,
+    control,    
+  } = useForm<ActivitySchema>({
+    mode: "onTouched",
+    resolver: zodResolver(activitySchema),
+  });
   const navigate = useNavigate();
+  const { id } = useParams();
+  const { updateActivity, createActivity, activity, isLoadingActivity } = UseActivities(id);
 
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+useEffect(() => {
+  if (activity) reset({
+    ...activity,
+      location: {
+        city: activity.city,
+        venue: activity.venue,
+        latitude: activity.latitude,
+        longitude: activity.longitude
+           }
+       });
+    }, [activity, reset]);
 
-    const formData = new FormData(event.currentTarget);
-    const data: { [key: string]: FormDataEntryValue } = {};
+  const onSubmit = async (data: ActivitySchema) => {
+    
+    const {location, ...rest} = data; 
+    const flattenedData = {...rest, ...location};
 
-    formData.forEach((value, key) => {
-      data[key] = value;
-    });
-
-    if (activity) {
-      data.id = activity.id;
-      await updateActivity.mutateAsync(data as unknown as Activity);
-      navigate(`/activities/${activity.id}`);
-    } else {
-      createActivity.mutate(data as unknown as Activity, {
-        onSuccess: (id) => {
-          navigate(`/activities/${id}`);
-        },
-      });
+      try {
+       if (activity) {
+         updateActivity.mutate({...activity, ...flattenedData}, {
+         onSuccess: () => navigate(`/activities/${activity.id}`)
+            })
+           } else {
+            createActivity.mutate(flattenedData, {
+            onSuccess: (id) => navigate(`/activities/${id}`)
+            })
+            }
+        } catch (error) {
+         console.log(error);
+        }
     }
-  };
 
   if (isLoadingActivity) return <Typography>Loading activity...</Typography>;
 
   return (
     <Paper sx={{ borderRadius: 3, padding: 3 }}>
       <Typography variant="h5" gutterBottom color="primary">
-         {activity ? 'Edit activity' : 'Create activity'}
+        {activity ? "Edit activity" : "Create activity"}
       </Typography>
       <Box
         component="form"
-        onSubmit={handleSubmit}
+        onSubmit={handleSubmit(onSubmit)}
         display="flex"
         flexDirection="column"
         gap={3}
       >
-        <TextField name="title" label="Title" defaultValue={activity?.title} />
-        <TextField
-          name="description"
-          label="Description"
-          defaultValue={activity?.description}
-          multiline
-          rows={3}
-        />
-        <TextField
-          name="category"
-          label="Category"
-          defaultValue={activity?.category}
-        />
-        <TextField
-          name="date"
-          label="Date"
-          type="date"
-          defaultValue={
-            activity?.date
-              ? new Date(activity.date).toISOString().split("T")[0]
-              : new Date().toISOString().split("T")[0]
-          }
-        />
-        <TextField name="city" label="City" defaultValue={activity?.city} />
-        <TextField name="venue" label="Venue" defaultValue={activity?.venue} />
+       <TextInput control={control} name="title" label="Title"/>
+       <TextInput control={control} name="description" label="Description" multiline rows={3}/>
 
+       <Box display='flex' gap={3}>
+         <SelectInput items={categoryOptions} control={control} name="category" label="Category"/> 
+         <DateTimeInput control={control} name="date" label="Date"/>    
+       </Box>
+          
+       <LocationInput control={control} name="location" label="Location"/>
+           
         <Box display="flex" justifyContent="end" gap={3}>
           <Button onClick={() => {}} color="inherit">
             Cancel
