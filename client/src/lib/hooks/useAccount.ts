8ee1559f6b90c73fb20e8af62de4a83906db9768
MarekVavrovic <vavrovic@gmail.com>
2025-11-18 10:27:50 +0000

    Password reset complete

--- client/src/lib/hooks/useAccount.ts
+++ client/src/lib/hooks/useAccount.ts
@@ -1,78 +1,110 @@
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
-import {type LoginSchema } from "../schemas/loginSchema"
-import agent from "../api/agent"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { type LoginSchema } from "../schemas/loginSchema";
+import agent from "../api/agent";
 import { useNavigate } from "react-router";
-import {type RegisterSchema } from "../schemas/registerSchema";
+import { type RegisterSchema } from "../schemas/registerSchema";
 import { toast } from "react-toastify";
+import type { ChangePasswordSchema } from "../schemas/changePasswordSchema";
 
 export const useAccount = () => {
-    const queryClient = useQueryClient();
-    const navigate = useNavigate();
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
 
-    const loginUser = useMutation({
-        mutationFn: async (creds: LoginSchema) => {
-            await agent.post('/login?useCookies=true', creds);
+  //user-info
+  const { data: currentUser, isLoading: loadingUserInfo } = useQuery({
+    queryKey: ["user"],
+    queryFn: async () => {
+      const response = await agent.get<User>("/account/user-info");
+      return response.data;
+    },
+    enabled: !queryClient.getQueryData(["user"]),
+  });
+
+  const loginUser = useMutation({
+    mutationFn: async (creds: LoginSchema) => {
+      await agent.post("/login?useCookies=true", creds);
+    },
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({
+        queryKey: ["user"],
+      });
+    },
+  });
+
+  const registerUser = useMutation({
+    mutationFn: async (creds: RegisterSchema) => {
+      await agent.post("/account/register", creds);
+    },
+  });
+
+  const logoutUser = useMutation({
+    mutationFn: async () => {
+      await agent.post("/account/logout");
+    },
+    onSuccess: () => {
+      queryClient.removeQueries({ queryKey: ["user"] });
+      queryClient.removeQueries({ queryKey: ["activities"] });
+      navigate("/");
+    },
+  });
+
+  const verifyEmail = useMutation({
+    mutationFn: async ({ userId, code }: { userId: string; code: string }) => {
+      await agent.get(`/confirmEmail?userId=${userId}&code=${code}`);
+    },
+  });
+
+  const resendConfirmationEmail = useMutation({
+    mutationFn: async ({
+      email,
+      userId,
+    }: {
+      email?: string;
+      userId?: string | null;
+    }) => {
+      await agent.get(`/account/resendConfirmEmail`, {
+        params: {
+          email,
+          userId,
         },
-        onSuccess: async () => {
-            await queryClient.invalidateQueries({
-                queryKey: ['user']
-            });
-        }
-    });
+      });
+    },
+    onSuccess: () => {
+      toast.success("Email sent - please check your email");
+    },
+  });
 
-    const registerUser = useMutation({
-        mutationFn: async (creds: RegisterSchema) => {
-            await agent.post('/account/register', creds)
-        }
-    })
+  //change password
+  const changePassword = useMutation({
+    mutationFn: async (data: ChangePasswordSchema) => {
+      await agent.post("/account/change-password", data);
+    },
+  });
 
-    const logoutUser = useMutation({
-        mutationFn: async () => {
-            await agent.post('/account/logout');
-        },
-        onSuccess: () => {
-            queryClient.removeQueries({queryKey: ['user']});
-            queryClient.removeQueries({queryKey: ['activities']});
-            navigate('/');
-        }
-    })
+  //forgot password
+  const forgotPassword = useMutation({
+    mutationFn: async (email: string) => {
+      await agent.post("/forgotPassword", { email });
+    },
+  });
 
-    const verifyEmail = useMutation({
-        mutationFn: async ({userId, code}: {userId: string, code: string}) => {
-            await agent.get(`/confirmEmail?userId=${userId}&code=${code}`)
-        }
-    });
+  //reset Password
+  const resetPassword = useMutation({
+    mutationFn: async (data: ResetPassword) => {
+      await agent.post("/resetPassword", data);
+    },
+  });
 
-    const resendConfirmationEmail = useMutation({
-        mutationFn: async ({email, userId}:{email?: string, userId?: string | null}) => {
-            await agent.get(`/account/resendConfirmEmail`, {
-                params: {
-                    email,
-                    userId
-                }
-            })
-        },
-        onSuccess: () => {
-            toast.success('Email sent - please check your email');
-        }
-    })
-
-    const {data: currentUser, isLoading: loadingUserInfo} = useQuery({
-        queryKey: ['user'],
-        queryFn: async () => {
-            const response = await agent.get<User>('/account/user-info');
-            return response.data;
-        },
-        enabled: !queryClient.getQueryData(['user']) 
-    })
-
-    return {
-        loginUser,
-        currentUser,
-        logoutUser,
-        loadingUserInfo,
-        registerUser,
-        verifyEmail,
-        resendConfirmationEmail
-    }
-}
+  return {
+    loginUser,
+    currentUser,
+    logoutUser,
+    loadingUserInfo,
+    registerUser,
+    verifyEmail,
+    resendConfirmationEmail,
+    changePassword,
+    forgotPassword,
+    resetPassword
+  };
+};
